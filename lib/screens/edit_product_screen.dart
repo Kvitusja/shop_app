18baecaa206c8e1c8224d4bc629ce46b9850b29a
natal <kvitusja@gmail.com>
2023-01-02 22:42:44 +0100

    'text field input method'

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -10,6 +10,17 @@ class EditProductScreen extends StatefulWidget {
 
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    super.dispose();
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,51 +32,39 @@ class _EditProductScreenState extends State<EditProductScreen> {
         child: Form(
           child: ListView(
             children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Product title',
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      width: 2,
-                    ),
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 2,
-                      color: Theme.of(context).colorScheme.primary,
-                    ),
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                ),
-                textInputAction: TextInputAction.next,
-                onFieldSubmitted: (_) {
+              buildTextFormField(
+                context,
+                TextInputAction.next,
+                (_) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
                 },
+                FocusNode(),
+                'Product title',
+                TextInputType.text,
               ),
               const SizedBox(
                 height: 10,
               ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Product price',
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      width: 1.5,
-                    ),
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 1.5,
-                      color: Theme.of(context).colorScheme.primary,
-                    ),
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                ),
-                textInputAction: TextInputAction.next,
-                keyboardType: TextInputType.number,
-                focusNode: _priceFocusNode,
+              buildTextFormField(
+                context,
+                TextInputAction.next,
+                (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocusNode);
+                },
+                _priceFocusNode,
+                'Product price',
+                TextInputType.number,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              buildTextFormField(
+                context,
+                TextInputAction.send,
+                (_) {},
+                _descriptionFocusNode,
+                'Description',
+                TextInputType.multiline,
               ),
             ],
           ),
@@ -73,4 +72,38 @@ class _EditProductScreenState extends State<EditProductScreen> {
       ),
     );
   }
+
+  buildTextFormField(
+      BuildContext context,
+      TextInputAction textInputAction,
+      void Function(String) onFiledSubmitted,
+      FocusNode focusNode,
+      String labelText,
+      TextInputType keyboardType) {
+    return TextFormField(
+        decoration: InputDecoration(
+          labelText: labelText,
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              width: 2,
+            ),
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              width: 2,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+        ),
+        textInputAction: textInputAction,
+        onFieldSubmitted: onFiledSubmitted,
+        focusNode: focusNode,
+        keyboardType: keyboardType);
+  }
 }
+
+// textInputAction: TextInputAction.next,
+// onFieldSubmitted: (_) {
+// FocusScope.of(context).requestFocus(_priceFocusNode);
